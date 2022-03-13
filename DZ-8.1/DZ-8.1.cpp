25dd4e1c0d53daa1983caec1b2901781e1577371
Хрусталёв Александр <Tikkirey90_90@mail.ru>
2022-03-13 16:27:14 +0300

    Финал. Прога работает.

--- DZ-8.1/DZ-8.1.cpp
+++ DZ-8.1/DZ-8.1.cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
+using namespace std;
+
+int Divider(int a, int b) {
+    
+    if (a % b == 0)
+        return b;
+    if (b % a == 0)
+        return a;
+    if (a > b)
+        return Divider (a % b, b);
+    return Divider (a, b % a);
+    
+    
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
+    int num1, num2;
+    cout << "Поиск наибольшего общего делителя двух целых чисел." << endl;
+    cout << "Введите первое целое число: ";
+    cin >> num1;
+    cout << endl;
+    cout << "Введите второе целое число: ";
+    cin >> num2;
+    cout << endl;
+    cout << "Результат = " << Divider(num1, num2);
 }
